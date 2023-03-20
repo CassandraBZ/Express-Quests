809b26495e1fdc1409564cff22f64ec86ecc9313
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-03-20 10:18:53 +0100

    quest 6

--- usersHandlers.js
+++ usersHandlers.js
@@ -4,18 +4,38 @@ const mysql = require("mysql2/promise");
 const database = require("./database");
 
 const getUsers = (req, res) => {
+  const initialSql = "select *, NULL AS hashedPassword from users";
+  const where = [];
+
+  if (req.query.city != null) {
+    where.push({
+      column: "city",
+      value: req.query.city,
+      operator: "=",
+    });
+  }
+  if (req.query.language != null) {
+    where.push({
+      column: "language",
+      value: req.query.language,
+      operator: "=",
+    });
+  }
+
   database
-
-    .query("select *, NULL AS hashedPassword from users")
-
+    .query(
+      where.reduce(
+        (sql, { column, operator }, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+        initialSql
+      ),
+      where.map(({ value }) => value)
+    )
     .then(([users]) => {
       res.json(users);
-      res.status(200);
     })
-
     .catch((err) => {
       console.error(err);
-
       res.status(500).send("Error retrieving data from database");
     });
 };
